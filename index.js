4095711932fdd1a8069a2457b4f1b2c6921fd22b
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-18 13:39:11 -0400

    randomness

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/start', (request, response) => {
 app.post('/move', (request, response) => {
   var data = request.body;
 
-   Choose a random direction to move in
+//Choose a random direction to move in
   possible_moves = ["up", "down", "left", "right"]
   var choice = Math.floor(Math.random() * possible_moves.length);
   var snake_move = possible_moves[choice];
